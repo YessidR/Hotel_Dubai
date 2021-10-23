5c288a804212cf7f7241de02fe01126643781664
YessidR <91134063+YessidR@users.noreply.github.com>
2021-10-22 22:49:44 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask,render_template,request, session, redirect, flash
+from markupsafe import escape
+import os
+import sqlite3
+from sqlite3 import Error
+
+#librerias hash
+import hashlib
+#from werkzeug.security import check_password_hash, generate_password_hash
+
 
 
 app = Flask(__name__)
@@ -11,63 +20,129 @@ def index():
 def login():
     return render_template ("login.html")
 
-@app.route("/new_user", methods=["GET", "POST"])
+@app.route("/register", methods=["GET"])
 def registrarse():
     return render_template ("new_user.html")
 
-@app.route("/buscar", methods=["GET"])
+#Yessid (Actualizacion de busqueda)
+@app.route("/buscar", methods=["GET","POST"])
 def buscar():
-    return render_template ("busquedaHab.html")
+    
+    if request.method == 'POST':   
+        options = request.form['tipo']
+        if options == "Buscar":
+            #Reemplazar con caja con alerta
+            return ('Debe seleccionar alguna opción')
+            
+        else:
+            tipo = int(options)
+            try:
+                print("Entramos al try")
+                with sqlite3.connect("dbh.db") as con:
+                    con.row_factory = sqlite3.Row
+                    cur = con.cursor()
+                    cur.execute("SELECT * FROM habitacion WHERE tipohabitacion = ? AND ocupado = 0", [tipo])
+                    row = cur.fetchall()
+                if row is None: 
+                    flash('No hay habitaciones disponibles, de acuerdo a tu búsqueda')
+                return render_template("busquedaHab.html", row=row)
+                # return tipo
+            except Error:
+                return "Fallo en conexion"
+    return "LLego hasta el final"
 
-@app.route("/res_vercom", methods=["GET", "POST"])
+#Diana (creado para hacer pruebas de habitación)
+@app.route("/reservar/<numHab>")
+def busqueda(numHab):
+    return render_template (str(numHab)+".html")
+
+
+
+#--------------------------------------------reservar:usuario final---------------------
+@app.route("/reservar/comentar", methods=["GET", "POST"])
 def res_vercom():
     return render_template ("res_vercom.html")
 
-@app.route("/reservar", methods=["GET", "POST"])
-def reservar():
+@app.route("/reservar/crear", methods=["GET", "POST"])
+def crear():
     return render_template ("reservar.html")
 
-@app.route("/comentarios", methods=["GET", "POST"])
+@app.route("/reservar/listar", methods=["GET", "POST"])
+def listar():
+    return render_template ("listaReserva.html")
+
+@app.route("/reservar/crear/comentarios", methods=["GET", "POST"])
 def comentarios():
-    return render_template ("comentarios.html")
-
-@app.route("/dashboard", methods=["GET", "POST"])
-def dashboard():
-    return render_template ("dashboard_index.html")
-
-@app.route("/agregar_hab", methods=["GET", "POST"])
-def agregar():
-    return render_template ("agregar_hab.html")
-
-@app.route("/calificar", methods=["GET", "POST"])
-def calificar():
     return render_template ("calificar.html")
 
-@app.route("/editar_hab", methods=["GET", "POST"])
-def editar():
-    return render_template ("editar_hab.html")
+@app.route("/reservar/comentarios/listar", methods=["GET", "POST"])
+def comentarios_listar():
+    return render_template ("comentarios.html")
 
-@app.route("/gestionarhabitaciones", methods=["GET", "POST"])
+#----------------------------------------admin/index------------------------------------
+@app.route("/admin/dashboard", methods=["GET", "POST"])
+def dashboard():
+    return render_template("dashboard_index.html")
+
+#---------------------------------------admin/habitaciones-------------------------------
+@app.route("/admin/dashboard/habitaciones", methods=["GET", "POST"])
 def gestionarH():
     return render_template ("gestionarhabitaciones.html")
 
-@app.route("/gestionarreserva", methods=["GET", "POST"])
-def gestionarR():
+@app.route("/admin/dashboard/habitaciones/agregar", methods=["GET", "POST"])
+def agregar():
+    return render_template ("agregar_hab.html")
+
+@app.route("/admin/dashboard/habitaciones/editar", methods=["GET", "POST"])
+def editar():
+    return render_template ("editar_hab.html")
+
+@app.route("/admin/dashboard/habitaciones/delete", methods=["GET", "POST"])
+def delete():
+    return render_template ("gestionarhabitaciones.html")
+
+#-----------------------------------------admin/Reservas------------------------------
+
+@app.route("/admin/dashboard/reservas/listar", methods=["GET", "POST"])
+def reserva_listar():
     return render_template ("gestionarreserva.html")
 
-@app.route("/gestionarusuario", methods=["GET", "POST"])
-def gestionarU():
+@app.route("/admin/dashboard/reservas/editar", methods=["GET", "POST"])
+def reserva_editar():
+    return render_template ("reservar.html")
+
+@app.route("/admin/dashboard/reservas/delete", methods=["GET", "POST"])
+def reserva_delete():
+    return render_template ("gestionarreserva.html")
+
+#-----------------------------------------admin/Usuarios-------------------------------------------------
+@app.route("/admin/dashboard/usuarios/listar", methods=["GET", "POST"])
+def usuario_listar():
+    if request.method == 'GET':
+        try:
+            with sqlite3.connect("dbh.db") as con:
+                con.row_factory = sqlite3.Row
+                cur = con.cursor()
+                cur.execute("SELECT nombre, idusuario FROM usuario WHERE rol IS NULL")
+                row = cur.fetchall()
+                if row is None:
+                    print('No hay registros')
+                return render_template ("gestionarusuario.html", row=row)
+        except Error:
+            print(Error)
+
+@app.route("/admin/dashboard/usuarios/editar", methods=["GET", "POST"])
+def usuario_editar():
+    return render_template ("new_user.html")
+
+@app.route("/admin/dashboard/usuarios/delete", methods=["GET", "POST"])
+def usuario_delete():
     return render_template ("gestionarusuario.html")
 
-@app.route("/li_reserva", methods=["GET", "POST"])
-def lista():
-    return render_template ("listaReserva.html")
-
-@app.route("/indexuser", methods=["GET","POST"])
+#seccion o ruta nueva
+@app.route("/index/logueado", methods=["GET","POST"])
 def indexuser():
     return render_template ("index_usuario.html")
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
